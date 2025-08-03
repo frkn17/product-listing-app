26a9a2e5ba87fea91ab5729c72d86af70f504636
Furkan Uyanık <mehmetfrkn07@gmail.com>
2025-08-03 21:42:43 +0300

    cors for deploying and testing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,10 +5,7 @@ import productRoutes from './routes/product.route'
 const app: Application = express();
 
 //Middleware
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-}));
+app.use(cors({ origin: "*" }))
 app.use(express.json());
 
 app.use('/products', productRoutes);
